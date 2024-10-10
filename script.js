dde7c6a8f355d288bea7976f91cb7c5d575a63cd
Mari-Lynn436 <marilynnm2@nycstudents.net>
2024-10-10 11:04:23 -0400

    Update from CodeDash on 10/10/2024, 11:04:20 AM

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add your JavaScript code here
+console.log('Welcome to your new project!');
+
+// Example: Change the text color when clicking on the heading
+document.querySelector('h1').addEventListener('click', function() {
+    this.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
+document.querySelector('p').addEventListener('click', function() {
+    this.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
